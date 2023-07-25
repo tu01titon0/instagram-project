23ce8d2483127dc65a285d037a150cfbb5ef2510
Van Vu <vanvn.vu@gmail.com>
2023-07-25 08:52:21 +0700

    Fixes

--- dist/src/controllers/post.controller.d.ts
+++
@@ -1,8 +0,0 @@
-export default class PostController {
-    static createPost(req: any, res: any): Promise<void>;
-    static getAllPosts(req: any, res: any): Promise<void>;
-    static getPostDetail(req: any, res: any): Promise<void>;
-    static postComment(req: any, res: any): Promise<any>;
-    static likeOrUnlike(req: any, res: any): Promise<void>;
-    static savePost(req: any, res: any): Promise<void>;
-}
